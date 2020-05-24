0f7d130e465691ba8c6f761c078c071f2b2ff4ac
yutingh <yutingh@cw.com.tw>
2020-05-25 00:19:11 +0800

    分兩版、調整順序

--- js/cwef.js
+++ js/cwef.js
@@ -1,8 +1,13 @@
 $(function () {
-    setInterval(function () {
-        $('.main__visual').children('div').removeClass('mask__group');
-    }, 5500);
-    setInterval(function () {
+    var mainVisualWidth = $('.main__visual--blank').outerWidth();
+    $('.mask__action svg').width(mainVisualWidth);
+    $(window).load(function () {
         $('.main__visual').children('div').addClass('mask__group');
-    }, 5600);
+        setInterval(function () {
+            $('.main__visual').children('div').removeClass('mask__group');
+        }, 5500);
+        setInterval(function () {
+            $('.main__visual').children('div').addClass('mask__group');
+        }, 5600);
+    })
 })
